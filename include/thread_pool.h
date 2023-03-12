149d10184bb1f9d9b1a86107903b9cf365c45981
Dawson Dittus <dawson.j.dittus.mil@army.mil>
2023-03-11 19:40:35 -0500

    Add signatures for future functions.

--- include/thread_pool.h
+++ include/thread_pool.h
@@ -6,7 +6,19 @@ extern "C"
 {
 #endif
 
-void *peek_tail(void);
+#include <stdint.h>
+#include <stddef.h>
+
+typedef void (*thread_pool_function_t)(void*);
+
+typedef struct thread_pool thread_pool_t;
+
+thread_pool_t* thread_pool_create(uint32_t num_threads);
+void thread_pool_destroy(thread_pool_t* pool);
+void thread_pool_add_work(thread_pool_t* pool, void* work, size_t size);
+void thread_pool_wait(thread_pool_t* pool);
+size_t thread_pool_get_size(thread_pool_t* pool);
+size_t thread_pool_get_pending_work(thread_pool_t* pool);
 
 
 #ifdef __cplusplus
--- src/thread_pool.c
+++ src/thread_pool.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
+#include <thread_pool.h>
+#include <pthread.h>
 
-int main(){
-    printf("hello");
-}
+struct thread_pool_ {
+    uint32_t num_threads;
+    pthread_t *threads;
+    pthread_mutex_t lock;
+    pthread_cond_t condition;
+    thread_pool_function_t function; 
+};
+
+thread_pool_t* thread_pool_create(uint32_t num_threads);
+void thread_pool_destroy(thread_pool_t* pool);
+void thread_pool_add_work(thread_pool_t* pool, void* work, size_t size);
+void thread_pool_wait(thread_pool_t* pool);
+size_t thread_pool_get_size(thread_pool_t* pool);
+size_t thread_pool_get_pending_work(thread_pool_t* pool);
